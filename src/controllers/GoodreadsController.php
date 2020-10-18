4a42cc4d9f9bcf9df37f52f7d9562208c68c6ba9
Nicolas Kupfer <jones.godel@gmail.com>
2020-10-18 02:50:07 +0200

    First commit for my new portfolio

---
+++ src/controllers/GoodreadsController.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * GoodreadsController seems a cool idea
+ */
+ class GoodreadsController extends Controller
+ {
+
+    /**
+     * @var GoodreadsController The class instance.
+     * @internal
+     */
+    protected static $instance;
+
+    protected $goodreadsService;
+
+    /**
+     * Returns a GoodreadsController instance, creating it if it did not exist.
+     * @return GoodreadsController
+     */
+    public static function singleton() {
+        if (!self::$instance) {
+            $v = __CLASS__;
+            self::$instance = new $v;
+        }
+        return self::$instance;
+    }
+
+    /**
+     * Constructor for the class GoodreadsController
+     */
+    public function __construct() {
+        parent::__construct();
+        $this->goodreadsService = GoodreadsService::singleton();
+    }
+
+    public function getCurrentlyReading () {
+        $books = $this->goodreadsService->getCurrentlyReading();
+//         var_dump($books);
+        $this->json($books);
+    }
+}
