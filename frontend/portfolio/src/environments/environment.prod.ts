7e1709df23f94f4d1f438e32f26abf536c181385
Nicolas Kupfer <jones.godel@gmail.com>
2020-10-22 01:31:08 +0200

    Login page

--- frontend/portfolio/src/environments/environment.prod.ts
+++ frontend/portfolio/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  url: 'http://portfolio.local/api'
+  // url: 'http://portfolio.local/api'
+  url: (window as any)._url_ || 'http://172.20.10.3/home-section.json?'
 };
