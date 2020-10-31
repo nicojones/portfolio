5bdca334c8d9d2656cda5f563ee6b204aa4f37ec
Nicolas Kupfer <jones.godel@gmail.com>
2020-10-31 16:48:23 +0100

    Changed API endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,8 +8,8 @@ import { Environment } from '~env/environment.interface';
 export const environment: Environment = {
   production: false,
   pageTitle: 'NK - Local',
-  phpUrl: 'https://nico.kupfer.es/api/app.php',
-  getUrl: 'https://nico.kupfer.es/json',
+  phpUrl: 'https://kupfer.es/portfolio-api/api/app.php',
+  getUrl: 'https://kupfer.es/portfolio-api/json',
   baseHref: '/'
 };
 
